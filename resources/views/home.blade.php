c515893405d1ff5f82e5e9bbf1831d8d36a8ebb6
Ayeshaalsidiq <ayesharijal69@gmail.com>
2025-10-17 16:45:24 +0700

    ubah

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -237,7 +237,7 @@
         </section>
     </main>
 
-    {{-- FOOTER --}}
+
     <footer class="bg-gray-800 text-gray-300">
         <div class="max-w-7xl mx-auto py-16 px-6">
             <div class="grid grid-cols-1 md:grid-cols-4 gap-10">
